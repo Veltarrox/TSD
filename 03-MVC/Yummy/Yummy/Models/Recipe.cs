2d774f2371a7f773160127a07a76b0565099a94f
Veltarrox <jasiuu55@wp.pl>
2022-04-06 19:20:12 +0200

    Sufficient

---
+++ 03-MVC/Yummy/Yummy/Models/Recipe.cs
@@ -0,0 +1,14 @@
+﻿namespace Yummy.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Time { get; set; }
+        public string Difficulty { get; set; }
+        public int NumberOfLikes { get; set; }
+        public string Ingredients { get; set; }
+        public string Process { get; set; }
+        public string TricksAndTips { get; set; }
+    }
+}
