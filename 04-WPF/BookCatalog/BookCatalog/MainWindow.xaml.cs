261e1bd51038fdcf9212e1b5f9cd263a342e2806
Veltarrox <jasiuu55@wp.pl>
2022-04-23 17:46:57 +0200

    Finishied sufficient

--- 04-WPF/BookCatalog/BookCatalog/MainWindow.xaml.cs
+++ 04-WPF/BookCatalog/BookCatalog/MainWindow.xaml.cs
@@ -19,8 +19,11 @@ namespace BookCatalog
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window
     {
+        public int idd = 0;
         public MainWindow()
         {
 
@@ -28,12 +31,36 @@ namespace BookCatalog
             InitializeComponent();
             BooksList.ItemsSource = books;
             cmbBooks.ItemsSource = Enum.GetValues(typeof(BookFormat));
-            
+            int tempid = 0;
+            foreach (Book book in (ObservableCollection<Book>)BooksList.ItemsSource)
+            {
+                if (book.Id > tempid)
+                {
+                    tempid = book.Id;
+                }
+            }
+            idd = tempid;
+
         }
         private void Delete_Click(object sender, RoutedEventArgs e)
         {
+            if (MessageBox.Show("Do you want to delete this record?",
+            "Confirmation", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                ((ObservableCollection<Book>)BooksList.ItemsSource).Remove((Book)BooksList.SelectedItem);
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            idd += 1;
+            Book temp = new Book(idd);
+            temp.IsRead = (bool)IsRead.IsChecked;
+            temp.Author = Author.Text;
+            if (Year.Text != "") 
+            temp.Year = int.Parse(Year.Text);
+            temp.Title = Title.Text;
+          
 
-            ((ObservableCollection<Book>)BooksList.ItemsSource).Remove((Book)BooksList.SelectedItem);
+            if(temp.Author != "" && temp.Year != null && temp.Title != "")
+                ((ObservableCollection<Book>)BooksList.ItemsSource).Add(temp);
         }
     }
 }
